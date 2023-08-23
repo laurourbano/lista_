4bc6a1193b649ba1d8cffbfb7c73f015e65ae5fa
laurourbano <lauro@outlook.com.br>
2023-08-22 23:44:57 -0300

    feat: incluindo routes, service, controller, db

---
+++ back/src/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use("/api", routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(() => {
+    console.log(`Server is running on port ${port}.`);
+})
--- back/src/db.js
+++ back/src/db.js
@@ -3,12 +3,14 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log('Connected to MySQL server.');
+    console.log(`Conectado ao banco de dados: ${process.env.DB_NAME}`);
 });
 
 module.exports = connection;
